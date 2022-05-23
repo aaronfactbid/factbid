226a995ee39fec99eacb4d413de344518a5bc440
hari-abacies <harikrishnan@abacies.com>
2022-05-24 00:28:28 +0530

    Response

--- wp-content/themes/customtheme/tabs-templates/about-template.php
+++ wp-content/themes/customtheme/tabs-templates/about-template.php
@@ -3,29 +3,43 @@
 ?>
 <div class="content-area">
     <div class="sub-cnt">
-        <h5 class="contents-heading">What is an acceptable claim:</h5>
         <?php
-            $acceptable_claim = get_post_meta($postId, "acceptable_claim", true);
-            echo apply_filters('the_content', $acceptable_claim);
+            $if_claimed = get_post_meta($postId, "if_claimed", true);
+            if($if_claimed){
         ?>
-    </div>
-
-    <div class="sub-cnt">
         <h5 class="contents-heading">What will it prove if claimed:</h5>
             <?php
-                $if_claimed = get_post_meta($postId, "if_claimed", true);
+                
                 echo apply_filters('the_content', $if_claimed);
+            }
             ?>
     </div>
 
     <div class="sub-cnt">
-        <h5 class="contents-heading">What will it prove if unclaimed:</h5>
         <?php
             $if_unclaimed = get_post_meta($postId, "if_unclaimed", true);
+            if($if_unclaimed){
+        ?>
+        <h5 class="contents-heading">What will it prove if unclaimed:</h5>
+        <?php
+            
             echo apply_filters('the_content', $if_unclaimed);
+            }
         ?>
     </div>
 
+    <div class="sub-cnt">
+        <?php
+             $acceptable_claim = get_post_meta($postId, "acceptable_claim", true);
+             if($acceptable_claim){
+        ?>
+        <h5 class="contents-heading">What is an acceptable claim:</h5>
+        <?php
+           
+            echo apply_filters('the_content', $acceptable_claim);
+        }
+        ?>
+    </div>
 
     <div class="sub-cnt ">
         <h5 class="contents-heading">Description:</h5>
