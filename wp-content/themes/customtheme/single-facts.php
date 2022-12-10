88bb58443ef9e9e76aa60b93267328df282f1408
aaronfactbid <aaron@factbid.org>
2022-12-10 19:47:27 -0300

    Start by removing the tabs and making bids and claims use page flow

--- wp-content/themes/customtheme/single-facts.php
+++ wp-content/themes/customtheme/single-facts.php
@@ -100,8 +100,28 @@
                   if($visibilty != 1){
                     $author_name = 'user';
                   }
-                  echo '<div class="meta-content"><p>'.$res[0]->id_factbid.' <small>'.$date. ' - ' . show_verified($author) . $author_name . ' </small></p>
-                '.display_rating($author). ' ' .$meta_data. '</div>';
+                echo '<div class="meta-content">'.$res[0]->id_factbid.' <small>'.$date. ' - ' . show_verified($author) . $author_name . ' </small>
+					'.display_rating($author). ' ' .$meta_data. '</div>';
+                echo '<div class="meta-content">Bids: ' . $total_bids . ' bids totalling $' . $total_bid_amount . ' <a href="view_bids">view bids</a> ';
+				if (is_user_logged_in()) {
+					echo ' Add my bid: <input type="text" id="name" name="name" size="7"> <input type="submit" value="add bid">';
+				}
+				else {
+					  echo '<a href="/sign-in">Login/signup to add a bid</a>';
+				}
+				echo '</div>';
+                echo '<div class="meta-content">Claims: ';
+				if( $total_claims > 0 ) {
+					echo $total_claims;
+				}
+				echo ' <a href="view_claims">view claims</a> ';
+				if (is_user_logged_in()) {
+					echo '<a href="view_claims">claim this</a>';
+				}
+				else {
+					  echo '<a href="/sign-in">Login/signup to claim this</a>';
+				}
+				echo '</div>';
                 ?>
             <div class="edit_post_links">
               <?php if (is_user_logged_in() && ($user_id == $post->post_author || current_user_can('administrator'))) { ?>
@@ -138,49 +158,7 @@
       </div>
     </div>
 
-
-  <div class="about-page-vew">
-    <ul class="nav nav-tabs" id="myTab" role="tablist">
-      <li class="nav-item" role="presentation">
-        <a class="nav-link active" id="about-tab" data-bs-toggle="tab" href="#about" role="tab" aria-controls="about" aria-selected="true">About</a>
-      </li>
-      <li class="nav-item <?php echo $tab_class; ?>" role="presentation">
-        <a class="nav-link" id="discuss-tab" <?php if($tab_status) { ?>data-bs-toggle="tab"<?php } ?> href="#discuss" role="tab" aria-controls="discuss" aria-selected="false">Discuss</a>
-      </li>
-      <li class="nav-item <?php echo $tab_class; ?>" role="presentation">
-        <a class="nav-link" id="bids-tab" <?php if($tab_status) { ?>data-bs-toggle="tab"<?php } ?> href="#bids" role="tab" aria-controls="bids" aria-selected="false">Bids</a>
-      </li>
-      <li class="nav-item <?php echo $tab_class; ?>" role="presentation">
-        <a class="nav-link" id="claims-tab" <?php if($tab_status) { ?>data-bs-toggle="tab"<?php } ?> href="#claims" role="tab" aria-controls="claims" aria-selected="false">Claims</a>
-      </li>
-      <li class="nav-item <?php echo $tab_class; ?>" role="presentation">
-        <a class="nav-link" id="related-tab" <?php if($tab_status) { ?>data-bs-toggle="tab"<?php } ?> href="#related" role="tab" aria-controls="related" aria-selected="false">Related</a>
-      </li>
-    </ul>
-    <div class="tab-content container" id="myTabContent">
-      
-        <div class="tab-pane fade show active container" id="about" role="tabpanel" aria-labelledby="about-tab">
-          <?php include( get_template_directory() . '/tabs-templates/about-template.php' ); ?>
-        </div>
-
-        <div class="tab-pane container fade" id="discuss" role="tabpanel" aria-labelledby="discuss-tab">
-          <?php include( get_template_directory() . '/tabs-templates/discuss-template.php' ); ?>
-        </div>
-
-        <div class="tab-pane container fade" id="bids" role="tabpanel" aria-labelledby="bids-tab">
-          <?php include( get_template_directory() . '/tabs-templates/bids-template.php' ); ?>
-        </div>
-        
-        <div class="tab-pane container fade" id="claims" role="tabpanel" aria-labelledby="claims-tab">
-          <?php include( get_template_directory() . '/tabs-templates/claims-template.php' ); ?>
-        </div>
-
-        <div class="tab-pane container fade" id="related" role="tabpanel" aria-labelledby="related-tab">
-          <?php include( get_template_directory() . '/tabs-templates/related-template.php' ); ?>
-        </div>
-      
-    </div>
-  </div>
+	<?php include( get_template_directory() . '/tabs-templates/about-template.php' ); ?>
 
 <?php
     endwhile;
